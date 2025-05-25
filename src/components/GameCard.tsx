d76cfceb3b76b7731aa67f00f268bb2dc6586562
Heni-Nm <anmi65492@gmail.com>
2025-05-25 16:44:04 -0700

    Add GenreList component and integrate it into App; refactor GameCard imports

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, Heading, Hide, HStack, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
